03ce186026f9511682d5af4a50c8e56c71f2f970
Lusparon <lusparon99@bk.ru>
2020-10-14 08:23:19 +0300

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace UnionPolygons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(116, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(844, 494);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить полигон";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Объединить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 512);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(844, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 541);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(843, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 575);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(839, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 46);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Очистка экрана";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 637);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnionPolygons
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        private PointF[] UNION = new PointF[0];
+        Pen pen_union = new Pen(Color.Red, 2);
+        private Graphics g;
+        private bool isDraw;    //переменные для событий
+        private PointF startPoint, endPoint = PointF.Empty;     //отрисовка линий
+        private PointF mainPoint = new PointF(0, 0);
+        private PointF minPolyPoint, maxPolyPoint;
+        private PointF[] polygon = new PointF[0];
+        private PointF[] general = new PointF[0];
+        bool flag = false;
+        PointF intersection = new PointF(-13, -13);
+        public Form1()
+        {
+            /*InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = (Bitmap)pictureBox1.Image;
+            Clear();
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            g.TranslateTransform(1, -1);*/
+
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = (Bitmap)pictureBox1.Image;
+            g = Graphics.FromImage(bmp);
+            isDraw = true;
+        }
+
+        private void Clear()
+        {
+            g.Clear(Color.White);
+            startPoint = endPoint = Point.Empty;
+            Array.Clear(polygon, 0, polygon.Length);
+            Array.Resize(ref polygon, 0);
+            intersection = new PointF(-1, -1);
+            general = new PointF[0];
+            polygon = new PointF[0];
+            UNION = new PointF[0];
+            //twoPolygons = false;
+            flag = false;
+
+            pictureBox1.Invalidate();
+        }
+
+        //List<Point> general = new List<Point>();
+        //List<Point> newPolygon = new List<Point>();
+        //bool twoPolygons = false;
+
+
+        #region Рисование полигонов
+        private void pictureBox1_MouseDown1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDraw)
+                {
+                    if (polygon.Length == 0)
+                    {
+                        minPolyPoint = maxPolyPoint = e.Location;
+                        startPoint = e.Location;
+                        Array.Resize(ref polygon, 1);
+                        polygon[0] = startPoint;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDraw)
+                {
+                    Array.Resize(ref polygon, polygon.Length + 1);
+
+                    if (endPoint.X < minPolyPoint.X)
+                    {
+                        minPolyPoint.X = endPoint.X;
+                    }
+                    if (endPoint.Y < minPolyPoint.Y)
+                    {
+                        minPolyPoint.Y = endPoint.Y;
+                    }
+                    if (endPoint.X > maxPolyPoint.X)
+                    {
+                        maxPolyPoint.X = endPoint.X;
+                    }
+                    if (endPoint.Y > maxPolyPoint.Y)
+                    {
+                        maxPolyPoint.Y = endPoint.Y;
+                    }
+
+                    polygon[polygon.Length - 1] = endPoint;
+                    startPoint = endPoint;
+                }
+            }
+            else
+            {
+                mainPoint.X = e.Location.X;
+                mainPoint.Y = e.Location.Y;
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDraw)
+                {
+                    endPoint = e.Location;
+                    pictureBox1.Invalidate();
+                    
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (general.Length > 1)
+                e.Graphics.DrawPolygon(Pens.Black, general);
+            if (polygon.Length > 1)
+                e.Graphics.DrawPolygon(Pens.Black, polygon);
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            if (general.Count() == 0){
+                general = polygon;
+                polygon = new PointF[0];
+                //flag = false;
+            }
+        }
+
+        //Принадлежит ли точка отрезку
+        private bool pointBelongsToLine(PointF p, PointF a, PointF b)
+        {
+            var x = Math.Round((p.Y - a.Y) / (b.Y - a.Y), 3);
+            var y = Math.Round((p.X - a.X) / (b.X - a.X), 3);
+            return  x == y;
+        }
+
+
+        //Функция берет список, стартовую точку в нем, ближайшее пересечение
+        //Действие - добавляет точки в список 
+        private void add_points(ref PointF[] src, PointF start, PointF intersection, bool direction) { 
+            PointF temp = start;
+            while (temp != intersection) {
+                Array.Resize(ref UNION, UNION.Length + 1);
+                UNION[UNION.Length - 1] = temp;
+                var nxt = next(ref src, temp);            
+                if (!pointBelongsToLine(intersection,temp,nxt))
+                    temp = nxt;
+                else temp = intersection;
+            }
+            Array.Resize(ref UNION, UNION.Length + 1);
+            UNION[UNION.Length - 1] = intersection;
+        }
+        
+        //Функция возвращает следущую точку массива, если конец -> возвращает начало
+        private PointF next(ref PointF[] lst, PointF p) {
+            int index = Array.IndexOf(lst, p);
+            PointF result = new PointF(0,0);
+
+            if (index == -1)
+            {
+                int n = lst.Length;
+                int i = 0;
+                do
+                {
+                    int next = (i + 1) % n;
+                    if (pointBelongsToLine(p, lst[i], lst[next]))
+                        return lst[next];
+
+                    i = next;
+                } while (i != 0);
+            }
+            else
+            {
+                if (index + 1 == lst.Count())
+                    result = lst[0];
+                else
+                    result = lst[index + 1];
+            }
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var general_min = general.OrderBy(x => x.X).ThenBy(x => x.Y).First();
+            var newPol_min = polygon.OrderBy(x => x.X).ThenBy(x => x.Y).First();
+            bool flag_pol = true;
+            var intersect = IntersectPoints(general,polygon);
+            //Array.Resize(ref general, intersect.Count());
+            //for (var i = general.Length - intersect.Count(); i < general.Length; i++ )
+             //   general[i]
+
+            PointF start_point = general.First();
+            PointF[] work_lst;
+
+            if (flag_pol)
+                work_lst = general;
+            else
+                work_lst = polygon;
+            PointF t = start_point;
+            bool direction = true;
+            foreach (var i in intersect){
+                add_points(ref work_lst, t, i, direction);
+                //PointF current_point;
+                if (flag_pol){
+                    PointF check_now = next(ref polygon, i);
+                    work_lst = polygon;
+                    flag_pol = false;
+                    t = check_now;
+                }
+                else {
+                    PointF check_now = next(ref general, i);
+                    work_lst = general;
+                    flag_pol = true;
+                    t = check_now;
+                }
+            }
+
+            if (flag_pol)
+            {
+                if (UNION.Count() == 0)
+                {
+                    Array.Resize(ref UNION, UNION.Length + 1);
+                    UNION[UNION.Length - 1] = next(ref general, general.First());
+                }
+                add_points(ref general, UNION.Last(), general.First(), true);
+            }
+            else
+            {
+                if (UNION.Count() == 0)
+                {
+                    Array.Resize(ref UNION, UNION.Length + 1);
+                    UNION[UNION.Length - 1] = polygon.First();
+                }
+                add_points(ref polygon, UNION.Last(), polygon.First(), true);
+            }
+            foreach (var i in UNION)
+                textBox1.Text += "(" + i.X + "; " + i.Y + ")   ";
+
+            g.DrawLines(pen_union, UNION.ToArray());
+            pictureBox1.Image = bmp;
+
+		}
+
+        private List<PointF> IntersectPoints(PointF[] general, PointF[] polygon)
+        {
+            var res = new List<PointF>();
+            int n = general.Length;
+            int m = polygon.Length;
+            int j = 0, i = 0;
+
+            do
+            {
+                int nextI = (i + 1) % n;
+                do
+                {
+                    int nextJ = (j + 1) % m;
+                    PointF intersection = Intersection(general[i], general[nextI],polygon[j], polygon[nextJ]);
+                    if (intersection.X != -1 && intersection.Y != -1)
+                    {
+                        Math.Round(intersection.X,0);
+                        Math.Round(intersection.Y, 0);
+                        res.Add(intersection);
+                    }
+                    j = nextJ;
+                } while (j != 0);
+
+                i = nextI;
+            } while (i != 0);
+
+
+
+            return res;
+        }
+        private PointF Intersection(PointF p0, PointF p1, PointF p2, PointF p3)
+        {
+            PointF i = new PointF(-1, -1);
+            PointF s1 = new PointF();
+            PointF s2 = new PointF();
+            s1.X = p1.X - p0.X;
+            s1.Y = p1.Y - p0.Y;
+            s2.X = p3.X - p2.X;
+            s2.Y = p3.Y - p2.Y;
+            float s, t;
+            s = (-s1.Y * (p0.X - p2.X) + s1.X * (p0.Y - p2.Y)) / (-s2.X * s1.Y + s1.X * s2.Y);
+            t = (s2.X * (p0.Y - p2.Y) - s2.Y * (p0.X - p2.X)) / (-s2.X * s1.Y + s1.X * s2.Y);
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+            {
+                i.X = p0.X + (t * s1.X);
+                i.Y = p0.Y + (t * s1.Y);
+
+            }
+            if (i == p0)
+                i = new PointF(-1, -1);
+            return i;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clear();            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*textBox1.Text = "";
+           foreach (var i in general)
+               textBox1.Text += "(" + i.X + "; " + i.Y + ")    ";
+
+           textBox2.Text = "";
+           foreach (var i in newPolygon)
+               textBox2.Text += "(" + i.X + "; " + i.Y + ")    ";
+
+           textBox3.Text = "";
+           foreach (var i in intersect)
+               textBox3.Text += "(" + i.X + "; " + i.Y + ")    ";*/
